f5d1eabe12c225f5fad83cd60501eace9f4c01cf
gabrielly-souza <gmdesouza20@gmail.com>
2023-07-22 18:56:32 -0300

    creating contact interface

---
+++ AgendaMvcProject/Application/Interfaces/IContactService.cs
@@ -0,0 +1,13 @@
+using Application.Models;
+
+namespace Application.interfaces
+{
+    public interface IContactService
+    {
+        Task<IEnumerable<Contact>> GetContactsAsync();
+        Task<Contact> GetContactByIdAsync(int id);
+        Task<Contact> AddContactAsync(Contact contact);
+        Task<Contact> UpdateContactAsync(Contact contact);
+        Task DeleteContactAsync(int id);
+    }
+}
