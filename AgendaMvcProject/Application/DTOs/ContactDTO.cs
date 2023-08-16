c6e84068aea66efcfec43963f43a28e3b83db472
gabrielly-souza <gmdesouza20@gmail.com>
2023-08-16 20:18:17 -0300

    fixing DTO

--- AgendaMvcProject/Application/DTOs/ContactDTO.cs
+++ AgendaMvcProject/Application/DTOs/ContactDTO.cs
@@ -5,16 +5,21 @@ namespace AgendaMvcProject.Application.DTOs;
 public class ContactDTO
 {
     public int Id { get; set; }
+
     [Required(ErrorMessage = "The Name is Required")]
     [MinLength(3)]
     [MaxLength(100)]
     public string? Name { get; set; }
-    [Required(ErrorMessage = "The Name is Required")]
-    public int Telephone { get; set; }
+
     [Required(ErrorMessage = "The Number is Required")]
     [MinLength(9)]
     [MaxLength(15)]
+    public int Telephone { get; set; }
+ 
+    [MaxLength(200)]
     public string? Description { get; set; }
+
+    [MaxLength(100)]
     public string? Email { get; set; }
 
 }
