9702d012249685b286146b9fb36e91b8232980b3
Murilo Ferreira <83832772+Nixssss@users.noreply.github.com>
2023-08-20 19:07:51 +0000

    creating service and interface

--- AgendaMvcProject/Application/Models/Address.cs
+++ AgendaMvcProject/Application/Models/Address.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace backend.Models;
+namespace Application.Models;
 [Table("Address")]
 public class Address
 {   
